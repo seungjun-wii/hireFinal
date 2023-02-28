79735cf4cfb65700b643b1727dd9765d4d2a6755
Seungjun Kim <imscott1009@gmail.com>
2023-02-28 21:42:13 +0900

    static settings

--- hire/settings.py
+++ hire/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # SASS Compiler
 SASS_PROCESSOR_ENABLED = True
